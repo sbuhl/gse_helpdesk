ae75baf5b3e1d3168aa546c612912924fc8e5950
Sébastien Bühl <buhlsebastien@gmail.com>
2022-09-21 18:04:19 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Helpdesk Custo',
+    'version': '0.1',
+    'category': 'Hidden',
+    'license': 'LGPL-3',
+    'summary': 'Helpdesk',
+    'description': """
+        Custo sur le helpdesk
+    """,
+    'depends': ['helpdesk'],
+    'data': [
+        'views/gse_helpdesk_form.xml',
+    ],
+    'installable': True,
+    
+}
---
+++ models/helpdesk.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    def action_new_quotation(self):
+        action = self.env.ref("sale_crm.sale_action_quotations_new").read()[0]
+        action['context'] = {
+            'search_default_partner_id': self.partner_id.id,
+            'default_partner_id': self.partner_id.id,
+            'default_campaign_id': self.campaign_id.id,
+            'default_medium_id': self.medium_id.id,
+            'default_origin': self.name,
+            'default_source_id': self.source_id.id,
+            'default_company_id': self.company_id.id or self.env.company.id,
+            'default_tag_ids': self.tag_ids.ids,
+        }
+        if self.team_id:
+            action['context']['default_team_id'] = self.team_id.id,
+        if self.user_id:
+            action['context']['default_user_id'] = self.user_id.id
+        return action
